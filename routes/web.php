7db957b59ba22cdbbb3e6eede355d6b2c92bb26d
GrahadiM <grahadim178@gmail.com>
2023-06-24 12:37:32 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function() {return redirect()->route('login');});
 
-
 Route::middleware(['auth'])->group(function () {
     Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'dashboard'])->name('dashboard');
 
